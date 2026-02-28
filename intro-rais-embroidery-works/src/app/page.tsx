4102f15d85474e2a090b63cd471079c55d0f2549
03musab <musabimp.0@gmail.com>
2026-03-01 02:54:22 +0530

    Fix nested repository issue for intro-rais-embroidery-works

---
+++ intro-rais-embroidery-works/src/app/page.tsx
@@ -0,0 +1,9 @@
+import RaisEmbroideryExperience from "@/components/RaisEmbroideryExperience";
+
+export default function Home() {
+  return (
+    <main className="w-full min-h-screen bg-[#050505]">
+      <RaisEmbroideryExperience />
+    </main>
+  );
+}
